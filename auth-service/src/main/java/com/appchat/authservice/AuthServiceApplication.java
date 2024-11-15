bfd2cb3ebf3d8fa65d6dcafe8e7315ca90d68c10
Patryk Likus <vhrk84@motorolasolutions.com>
2024-11-15 22:36:50 +0100

    add auth service boilerplate

--- auth-service/src/main/java/com/appchat/authservice/AuthServiceApplication.java
+++ auth-service/src/main/java/com/appchat/authservice/AuthServiceApplication.java
@@ -1,9 +1,11 @@
-package com.chatwave.authservice;
+package com.appchat.authservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories
 public class AuthServiceApplication {
   public static void main(String[] args) {
     SpringApplication.run(AuthServiceApplication.class, args);
